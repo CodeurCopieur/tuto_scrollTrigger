fc5d2716f76c08c63d9d1a251cdf57f37bcd7f44
codwerk <louis_widdy@live.fr>
2021-03-27 19:09:59 +0100

    Start and End

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
 gsap.registerPlugin(ScrollTrigger)
 
-gsap.to(".square", {
+/*gsap.to(".square", {
   x: 700,
   duration: 3,
   scrollTrigger: ".square2"
+})*/
+
+
+gsap.to(".square", {
+  //x: 700,
+  duration: 3,
+  scrollTrigger: {
+    trigger: ".square",
+    start: "top 50%",
+    end: () =>  `+=${document.querySelector(".square").offsetHeight}`,//"center 20%",
+    markers: true,
+    toggleClass: "toggleClass"
+    /*markers: {//ou true
+      startColor: "blue",
+      endColor: "red",
+      fontSize: "10px",
+      indent: 20
+    }*/
+  }
 })
